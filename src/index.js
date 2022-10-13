58e9f395430a8549ed74329ee07e3eaefabd911c
Ahmed Elwerdany <infoalwerdany@gmail.com>
2022-10-14 01:43:44 +0200

    use `locator.loc` method

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-export default function(context, options = {}) {
-    const {Syntax, RuleError, report, getSource, locator} = context;
-    return {
-        [Syntax.Code](node) {
-            const match = node.value.trim() === ''  ? true : false
-            // const text = context.getSource(node);
-            // const match = text.match(/\[\s+\]\s/i);
-            if (match) {
-                report(
-                    node,
-                    new context.RuleError(`Empty code block`, {
-                        padding: locator.range([node.range[0], node.range[1]])
-                    })
-                );
-            }
-        }
-    }
-};
+export default function (context, options = {}) {
+  const { Syntax, RuleError, report, getSource, locator } = context;
+  return {
+    [Syntax.Code](node) {
+      const match = node.value.trim() === "" ? true : false;
+      // const text = context.getSource(node);
+      // const match = text.match(/\[\s+\]\s/i);
+      if (match) {
+        const {
+          loc: { start, end },
+        } = node;
+
+        report(
+          node,
+          new context.RuleError(`Empty code block`, {
+            padding: locator.loc({
+              start: { line: start.line, column: start.column },
+              end: { line: end.line, column: end.column },
+            }),
+          })
+        );
+      }
+    },
+  };
+}
